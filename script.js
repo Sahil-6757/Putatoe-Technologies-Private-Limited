21ad0adc97364e9bca8347df359ee3b0de5dd6cd
Arbaz khan <Arbaz151033@gmail.com>
2022-11-10 14:08:31 +0500

    update javascript file

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let voice = document.getElementById("voice");
-let serach = document.getElementById('serach');
+let serach = document.getElementById("serach");
 voice.addEventListener("click", (e) => {
-alert('Your voice recognition is started');
-serach.value = '';
+  alert("Your voice recognition is started");
+  serach.value = "";
   let recognition = new webkitSpeechRecognition();
 
   recognition.lang = "en-GB";
-  recognition.onresult = function (e){
-  
+  recognition.onresult = function (e) {
     serach.value = e.results[0][0].transcript;
-  }
-recognition.start();
-
+  };
+  recognition.start();
 });
